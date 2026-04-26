c8cd64b534423a682e67fe41740fcfbef6bb2192
pinapel_dev321 <hussaynjebril@gmail.com>
2026-04-26 18:16:12 +0700

    add the files

---
+++ vvtl-vvtiny.sh
@@ -0,0 +1,1 @@
+echo VVTL;echo type h for help;while :;do printf ">";read c;case $c in h)echo d=clear v=version x=exit p=path r=read t=text;;d)clear;;v)echo 1.0.0;;x)break;;p)pwd;;r)ls;;t)vi 1.txt;;*)echo bad!;;esac;done
